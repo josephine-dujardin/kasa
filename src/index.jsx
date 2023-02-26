141cfab6dd8ed48d5524fa5633f2df752b395d03
DUJARDIN Joséphine <josephine83230@gmail.com>
2023-02-26 19:56:40 +0100

    web-app deployed to gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <Router basename="/">
     <GlobalStyle />
     <Header />
     <Routes>
