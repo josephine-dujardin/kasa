f03dd8c12285115046e9d8334780a4359b519ef9
DUJARDIN Joséphine <josephine83230@gmail.com>
2023-02-27 22:19:15 +0100

    fix bug deployment-10

--- src/utils/hooks.jsx
+++ src/utils/hooks.jsx
@@ -5,7 +5,7 @@ export function Hooks() {
     const [data, setData] = useState([]);
 
     const getData = () => {
-        fetch('http://localhost:3000/data.json'
+        fetch('https://josephine-dujardin.github.io/kasa/data.json'
             , {
                 headers: {
                     'Content-Type': 'application/json',
