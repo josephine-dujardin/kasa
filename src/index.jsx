9d0df4f10f3b29b4f7aa804086fcdca9e8f4b7fe
DUJARDIN Joséphine <josephine83230@gmail.com>
2023-02-27 20:08:25 +0100

    fix bug deployment-4

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/">
+  <Router basename="/kasa">
     <GlobalStyle />
     <Header />
     <Routes>
